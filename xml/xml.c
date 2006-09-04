7ee9f52075591add52019104edb6ba0dfb3d21d0
lordjaxom <lordjaxom>
2006-09-04 20:48:57 +0000

    - fixed possible buffer overflow in xml reading

--- xml/xml.c
+++ xml/xml.c
@@ -1,5 +1,5 @@
 /*
- *  $Id: xml.c,v 1.1 2004/12/19 22:03:49 lordjaxom Exp $
+ *  $Id: xml.c,v 1.2 2006/09/04 20:48:57 lordjaxom Exp $
  *  This module was kindly provided by Clemens Kirchgatterer
  */
 
@@ -38,7 +38,7 @@ XML::XML(const string &file) {
 	buffer = new char [size];
 	f.read(buffer, size);
 	f.close();
-	data = buffer;
+	data ssign(buffer, size);
 	delete[] buffer;
 	nodestartcb  = NULL;
 	nodeendcb    = NULL;
