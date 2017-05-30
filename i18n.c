79022b11e7ba817550dee1a6e59a8d06e6a1d2a9
Frank Neumann <fnu@yavdr.org>
2017-05-30 17:07:06 +0200

    Improve fix for c++11 compiler warning.

--- i18n.c
+++ i18n.c
@@ -4,7 +4,7 @@
 #include <vdr/config.h>
 
 cText2SkinI18n::cText2SkinI18n(const char *Skin) :
-	mIdentity(std::string( "vdr-"PLUGIN_NAME_I18N"-" ) + Skin)
+	mIdentity(std::string(" vdr- " PLUGIN_NAME_I18N " - ") + Skin)
 {
 	I18nRegister(mIdentity.substr(mIdentity.find('-') + 1).c_str());
 }
